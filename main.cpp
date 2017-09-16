6bd41ac1ed26eac44337750cdb309674ee19804d
Heri Agape <agape@live.fr>
2017-09-16 20:40:39 +0300

    some lines

--- main.cpp
+++ main.cpp
@@ -54,7 +54,7 @@ int main (int argc , char** argv)
     glutInitWindowPosition (50,100);
     glutInitWindowSize(500,500);
     glutCreateWindow ("Shape Point");
-
+    
     init();
     glutDisplayFunc(shapepoint);
     glutMainLoop();
