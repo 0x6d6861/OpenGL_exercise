543794b463bd3a18eb3c8c46acc0ce11835315fe
Heri Agape <agape@live.fr>
2017-09-19 23:11:47 +0300

    some compile configs

---
+++ cmake-build-debug/CMakeFiles/Opengl.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Opengl.dir/main.cpp.o"
+  "CMakeFiles/Opengl.dir/src/source/Point.cpp.o"
+  "Opengl.pdb"
+  "Opengl"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Opengl.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
