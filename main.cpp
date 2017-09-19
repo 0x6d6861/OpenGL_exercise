8a698ce2a48cc115518cdcd2ebae977bff3ceb6b
Heri Agape <agape@live.fr>
2017-09-19 22:29:41 +0300

    Implemetation of Bresenham algorithm, initial commit

--- main.cpp
+++ main.cpp
@@ -1,40 +1,36 @@
 #include <GL/glut.h>
 #include <cstdio>
-#include "src/Point.h"
 
+struct Point {
+    GLint x, y;
+};
+
+void bresenham(Point a, Point b);
+void draw_axis(int min_x, int max_x, int min_y, int max_y);
 
 void init () {
-    glClearColor (1.0,1.0,1.0,1.0);
-    gluOrtho2D (-1.5,1.5,-1.5,1.5);
+    gluOrtho2D (-15,65,-15,65);
+    glClearColor(0.0f,0.0f,0.0f,0.2f);
+    glClear(GL_COLOR_BUFFER_BIT);
 }
+
+
 
 void shapepoint (){
 
-    GLfloat psize = 10.0;
+    Point point1, point2;
 
-    Color pColors[6][3] = {
-            {0.0, 0.000, 1.000},
-            {0.647, 0.165, 0.165},
-            {0.0, 1.000, 1.000},
-            {0.0, 1.0, 0.000},
-            {1.0, 0.000, 1.0},
-            {1.0, 0.000, 0.000}
-    };
-    Position pCordinate[6][2] = {
-            {0,-1},
-            {-0.87,-0.5},
-            {-0.87,0.5},
-            {0.0,1.0},
-            {0.87,0.5},
-            {0.87,-0.5}
-    };
+    point1.x = 4;
+    point1.y = 2;
 
-    glClear (GL_COLOR_BUFFER_BIT);
+    point2.x = 10;
+    point2.y = 6;
 
-    for(int i = 0; i < 6; ++i){ // TODO: Point by the size of point
-        Point myPoint = Point(pCordinate[i], pColors[i], psize);
-        myPoint.draw_point();
-    }
+
+    draw_axis(-10, 60, -10, 60);
+
+    bresenham( point1, point2);
+
     glFlush();//enables fast execution of statements
 
 }
@@ -50,4 +46,99 @@ int main (int argc , char** argv)
     glutDisplayFunc(shapepoint);
     glutMainLoop();
     return 1;
+}
+
+
+void bresenham(Point a, Point b){
+    GLint x = a.x;
+    GLint y = a.y;
+    GLint dx = b.x - a.x;
+    GLint dy = b.y - a.y;
+    GLfloat m = GLfloat(dy) / GLfloat(dx);
+    GLint steps = (abs(dx) > abs(dy)) ? abs(dx) : abs(dy);
+    steps++;
+    GLint Pk = (m > 1) ? 2*dx - dy : 2*dy - dx;
+    glColor3f(0.0, 1.0, 0.0);
+    glPointSize(10.0);
+    glBegin(GL_POINTS);
+        do{
+            printf("%i | %i\n",x, y);
+            glVertex2d(x, y); // Plots the first point
+            if(Pk > 0){
+                x += 1;
+                y += 1;
+                Pk += (m > 1) ? 2*dx - 2*dy : 2*dy - 2*dx;
+            }else{
+                x += (m > 1) ? 0 : 1;
+                y += (m > 1) ? 1 : 0;
+                Pk += (m > 1) ? 2*dx  : 2*dy;
+            }
+            --steps;
+        }while(steps);
+    glEnd();
+    glFlush();
+
+}
+
+
+void draw_axis(int min_x, int max_x, int min_y, int max_y){
+    // The x-axis
+    glLineWidth(.7);
+    glColor3f(0.282, 0.239, 0.545);
+    glBegin(GL_LINES);
+    glVertex3f(min_x, 0.0, 0.0);
+    glVertex3f(max_x, 0, 0);
+    glEnd();
+
+    // The y-axis
+    glLineWidth(1.0);
+    glColor3f(0.282, 0.239, 0.545);
+    glBegin(GL_LINES);
+    glVertex3f(0, min_y, 0.0);
+    glVertex3f(0, max_y, 0);
+    glEnd();
+
+
+    // x-axis
+    for(int i = (min_x + 4); i <= (max_x - 10); i+=2){
+
+        // x-axis
+        glColor3f(1.0, 1.0, 0.0);
+        glBegin(GL_LINES);
+        glVertex3f(i, -.5, 0.0);
+        glVertex3f(i, .5, 0);
+        glEnd();
+
+
+        if(i > 0) {
+            glColor3f(0.1, 0.1, 0.1);
+            glBegin(GL_LINES);
+            glVertex3f(i, 2, 0.0);
+            glVertex3f(i, (max_y - 10), 0);
+            glEnd();
+
+        }
+
+    }
+
+    // y-axis
+    for(int i = (min_y + 4); i <= (max_y - 10); i+=2){
+        // y-axis
+        glColor3f(1.0, 1.0, 0.0);
+        glBegin(GL_LINES);
+        glVertex3f(-0.5f, i, 0.0f);
+        glVertex3f(0.5f, i, 0.0f);
+        glEnd();
+
+        if(i > 0) {
+            glColor3f(0.1f, 0.1f, 0.1f);
+            glBegin(GL_LINES);
+            glVertex3f(2, i, 0.0f);
+            glVertex3f((max_x - 10), i, 0.0f);
+            glEnd();
+        }
+
+    }
+    glFlush();
+
 }
